a276a73b340bcc509c5f5c612545a053004282b4
Mukul Howale <howalemukul@gmail.com>
2021-11-28 02:47:48 +0530

    added styling to home page and skills page

--- components/homeComp.js
+++ components/homeComp.js
@@ -7,6 +7,17 @@ let home = () =>{
         I love working on backend systems
         and would love to have a chat with you!
     </p>
+    <div>
+        <a href="">
+            <span class="iconify" data-icon="akar-icons:github-fill" style="color: black;" data-width="30" data-height="30"></span>
+        </a>
+        <a href="https://www.linkedin.com/in/mukulhowale">
+            <span class="iconify" data-icon="akar-icons:linkedin-fill" style="color: black;" data-width="30" data-height="30"></span>
+        </a>
+        <a href="https://drive.google.com/file/d/1zOqKSmfIx0CzukWf7KEYAHf8h2O0ZgqW/view?usp=sharing" style="text-decoration: none;">
+            <span><div>RESUME</div></span>
+        </a>
+    </div>
 </div>
 <div id="body_right">
     <img src="../images/20211015_121240.jpg" alt="">
