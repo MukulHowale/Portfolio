552e27448bb83fe7f4d809fd51424a785587adfc
Mukul Howale <howalemukul@gmail.com>
2021-11-28 00:59:47 +0530

    home page update

---
+++ components/homeComp.js
@@ -0,0 +1,16 @@
+let home = () =>{
+    return `<div id="body_left">
+    <div>Hello!</div>
+    <div>I'm Mukul Howale</div>
+    <div>I'm a BackEnd Developer</div>
+    <p>
+        I love working on backend systems
+        and would love to have a chat with you!
+    </p>
+</div>
+<div id="body_right">
+    <img src="../images/20211015_121240.jpg" alt="">
+</div>`
+}
+
+export default home;
