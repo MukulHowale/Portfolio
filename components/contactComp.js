8c8436cb6a1d02adb3f45a0b162b5701ff8ae4ff
Mukul Howale <howalemukul@gmail.com>
2021-11-28 19:51:02 +0530

    update on project and contacts page

---
+++ components/contactComp.js
@@ -0,0 +1,25 @@
+let contact = () =>{
+    return`<div id="c">
+    <div>
+        <div>Let's</div>
+        <div>Collaborate!</div>
+        <p>
+            Working in an collaborative environment is the best 
+            and I love it. If you have some work for me, we can 
+            get in touch. 
+        </p>
+    </div>
+    <div>
+        <a href="https://github.com/MukulHowale">
+            <span class="iconify" data-icon="akar-icons:github-fill" style="color: black;" data-width="30" data-height="30"></span>
+        </a>
+        <a href="https://www.linkedin.com/in/mukulhowale">
+            <span class="iconify" data-icon="akar-icons:linkedin-fill" style="color: black;" data-width="30" data-height="30"></span>
+        </a>
+        <span class="iconify" data-icon="mdi:gmail" style="color: black;" data-width="30" data-height="30"></span>
+        <span class="iconify" data-icon="el:phone-alt" style="color: black;" data-width="30" data-height="30"></span>
+    </div>
+</div>`
+}
+
+export default contact;
