c0c5e0233d4ce11db5cb396c6d245192636c617b
Mukul Howale <howalemukul@gmail.com>
2021-12-02 21:33:18 +0530

    added about component

---
+++ components/aboutComp.js
@@ -0,0 +1,24 @@
+let about = () =>{
+    return`<div id="a">
+    <span>Hello again!</span> Let me introduce myself again, my name is
+    Mukul Howale and I have a hands on experience working 
+    on both front-end and back-end parts of application.
+    <br>
+    <br>
+    <span>But I love to work on back-end part the most!</span>
+    <br>
+    <br>
+    When I wrote my first program, I was really exicited to see the
+    result. That made me interested to learn programming. Since then,
+    I think I have come a long way and it is a long journey ahead and
+    I am enjoying every part of it.
+    <br>
+    <br>
+    <span>
+        Other than coding, my interest lies in playing video games, listening
+        to music, watching anime and cooking.
+    </span>
+</div>`
+}
+
+export default about;
